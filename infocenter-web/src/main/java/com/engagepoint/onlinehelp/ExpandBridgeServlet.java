5aa6cca54cb1e65e4ffcafb0935916facf3f7318
Yurii Kukharskyi <yurii.kukharskyi@engagepoint.com>
2014-07-02 11:52:34 +0300

    Fix deploy on LTE

--- infocenter-web/src/main/java/com/engagepoint/onlinehelp/ExpandBridgeServlet.java
+++ infocenter-web/src/main/java/com/engagepoint/onlinehelp/ExpandBridgeServlet.java
@@ -3,24 +3,42 @@ package com.engagepoint.onlinehelp;
 import org.eclipse.equinox.servletbridge.BridgeServlet;
 import org.eclipse.equinox.servletbridge.FrameworkLauncher;
 
+import javax.servlet.ServletException;
 import java.lang.reflect.Field;
 
 /**
  * Created by ykukharskyi on 01.07.14.
  */
 public class ExpandBridgeServlet extends BridgeServlet {
+    FrameworkLauncher frameworkLauncher;
+
+//    @Override
     public void destroy() {
-        try {
-            Field framework = super.getClass().getDeclaredField("framework");
-            framework.setAccessible(true);
-            FrameworkLauncher frameworkLauncher = new FrameworkLauncher();
-            framework.get(frameworkLauncher);
+        if (frameworkLauncher != null) {
             frameworkLauncher.undeploy();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
         }
+//        try {
+//            Field framework = super.getClass().getDeclaredField("framework");
+//            framework.setAccessible(true);
+//            FrameworkLauncher frameworkLauncher = new FrameworkLauncher();
+//            undeploy((FrameworkLauncher) framework.get(frameworkLauncher));
+//            frameworkLauncher.undeploy();
+//        } catch (NoSuchFieldException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
         super.destroy();
     }
+
+//    @Override
+    public void init() throws ServletException {
+        frameworkLauncher = new FrameworkLauncher();
+        frameworkLauncher.undeploy();
+        super.init();
+    }
+
+//    private void undeploy(FrameworkLauncher fl) {
+//        fl.undeploy();
+//    }
 }
