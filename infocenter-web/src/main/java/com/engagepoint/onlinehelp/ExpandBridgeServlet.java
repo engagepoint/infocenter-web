fb6398306558204f57ca3a21ce0473b84ad32daa
Yurii Kukharskyi <yurii.kukharskyi@engagepoint.com>
2014-07-02 14:40:11 +0300

    Fix deploy on LTE - fix add redeploy to init

--- infocenter-web/src/main/java/com/engagepoint/onlinehelp/ExpandBridgeServlet.java
+++ infocenter-web/src/main/java/com/engagepoint/onlinehelp/ExpandBridgeServlet.java
@@ -12,33 +12,37 @@ import java.lang.reflect.Field;
 public class ExpandBridgeServlet extends BridgeServlet {
     FrameworkLauncher frameworkLauncher;
 
-//    @Override
+    @Override
     public void destroy() {
         if (frameworkLauncher != null) {
-            frameworkLauncher.undeploy();
+            undeploy();
         }
-//        try {
-//            Field framework = super.getClass().getDeclaredField("framework");
-//            framework.setAccessible(true);
-//            FrameworkLauncher frameworkLauncher = new FrameworkLauncher();
-//            undeploy((FrameworkLauncher) framework.get(frameworkLauncher));
-//            frameworkLauncher.undeploy();
-//        } catch (NoSuchFieldException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
-        super.destroy();
     }
 
-//    @Override
+    @Override
     public void init() throws ServletException {
-        frameworkLauncher = new FrameworkLauncher();
-        frameworkLauncher.undeploy();
         super.init();
+        try {
+            Field framework = super.getClass().getDeclaredField("framework");
+            framework.setAccessible(true);
+            frameworkLauncher = new FrameworkLauncher();
+            frameworkLauncher = (FrameworkLauncher) framework.get(frameworkLauncher);
+            redeploy();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
-//    private void undeploy(FrameworkLauncher fl) {
-//        fl.undeploy();
-//    }
+    private void undeploy() {
+        frameworkLauncher.undeploy();
+    }
+
+    private void redeploy() {
+        frameworkLauncher.stop();
+        undeploy();
+        frameworkLauncher.deploy();
+        frameworkLauncher.start();
+    }
 }
