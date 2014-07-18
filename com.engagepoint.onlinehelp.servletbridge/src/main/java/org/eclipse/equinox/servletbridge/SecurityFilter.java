da5e09f982a33319b9dbda7fad867717c1724b1a
Yaroslav Nikolaiko <yaroslav.nikolaiko@EngagePoint.com>
2014-07-18 15:04:55 +0300

    security

--- com.engagepoint.onlinehelp.servletbridge/src/main/java/org/eclipse/equinox/servletbridge/SecurityFilter.java
+++ com.engagepoint.onlinehelp.servletbridge/src/main/java/org/eclipse/equinox/servletbridge/SecurityFilter.java
@@ -14,10 +14,14 @@ import java.util.*;
 public class SecurityFilter implements Filter {
     private static Set<String> availableRoles = new HashSet<String>();
     private static ThreadLocal<String> role = new ThreadLocal<String>();
+    private static InputStream rolesStream;
+    private static final String ROLE_FILE_PATH = "/WEB-INF/roles.properties";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        readAllRoles(filterConfig.getServletContext());
+        if(rolesStream == null)
+            rolesStream = filterConfig.getServletContext().getResourceAsStream(ROLE_FILE_PATH);
+        readAllRoles();
     }
 
     @Override
@@ -25,7 +29,7 @@ public class SecurityFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         if(req.getUserPrincipal()!=null)
             System.out.println("Inside SecurityFilter Principal "+req.getUserPrincipal().getName() );
-        System.out.println("Inside SecurityFilter isUserInRole onlinehelpadmin"+req.isUserInRole("onlinehelpadmin") );
+        System.out.println("Inside SecurityFilter isUserInRole onlinehelpadmin  ->"+req.isUserInRole("onlinehelpadmin") );
 
         String userRole = null;
         for (String availableRole : availableRoles) {
@@ -38,39 +42,50 @@ public class SecurityFilter implements Filter {
 
     @Override
     public void destroy() {
-
-    }
-
-    private void readAllRoles(ServletContext context) {
-        String filepath = "/WEB-INF/roles.properties";
-        Properties property = new Properties();
-        InputStream input = null;
-        try {
-            input = context.getResourceAsStream(filepath);
-            if(input==null)
-                return;
-            property.load(input);
-        } catch (IOException ex) {
-            //throw new IOException(MessageFormat.format("Problem with loading default configurations file {0} ", filename), ex);
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    //logger.warn(MessageFormat.format("Failed to close {0} file", filename), e);
-                }
+        if(rolesStream != null){
+            try {
+                rolesStream.close();
+            } catch (IOException e) {
+                //logger.warn(MessageFormat.format("Failed to close {0} file", filename), e);
             }
         }
+    }
+
+    private void readAllRoles() {
+        Properties property = readRolesProperties(rolesStream);
         for (Object role : property.values()) {
             availableRoles.addAll(parseRolesValue(role.toString()));
         }
     }
 
-    private List<String> parseRolesValue(String value) {
-        return Arrays.asList(value.split(","));
+    private static Set<String> parseRolesValue(String value) {
+        return new HashSet<String>(Arrays.asList(value.split(",")));
+    }
+
+    private static Properties readRolesProperties(InputStream input){
+        Properties property = new Properties();
+        if(input==null)
+            return property;
+        try {
+            property.load(input);
+        } catch (IOException ex) {
+            //throw new IOException(MessageFormat.format("Problem with loading default configurations file {0} ", filename), ex);
+        }
+        return property;
     }
 
     public static String getRole(){
         return role.get();
+    }
+
+    public static Map<String, Set<String>> getLabelRolesMap() {
+        Properties property = readRolesProperties(rolesStream);
+        Map<String, Set<String>> labelRolesMap = new HashMap<String, Set<String>>();
+
+        for (Object o : property.keySet()) {
+            String key = o.toString();
+            labelRolesMap.put(key, parseRolesValue(property.getProperty(key)));
+        }
+        return labelRolesMap;
     }
 }
